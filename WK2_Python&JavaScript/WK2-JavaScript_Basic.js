73c1087df93fe8168f8fcbb251ce4cc83b7caf1f
zxcvbn848 <70138536+zxcvbn848@users.noreply.github.com>
2021-03-01 12:14:06 +0800

    Add files via upload

--- WK2_Python&JavaScript/WK2-JavaScript_Basic.js
+++ WK2_Python&JavaScript/WK2-JavaScript_Basic.js
@@ -1,6 +1,5 @@
 // 要求一：函式與流程控制
 // 完成以下函式，在函式中 使用迴圈 計算最小值到最大值之間，所有整數的總和。
-
 function calculate(min, max) {
     let sum = 0;
     for (let n = min; n <= max; n++) {
@@ -11,13 +10,17 @@ function calculate(min, max) {
 
 calculate(1, 3); // 要能夠計算 1 + 2 + 3，最後印出 6
 calculate(4, 8); // 要能夠計算 4 + 5 + 6 + 7 + 8，最後印出 30
-
 // ===============================
 // 要求二：Python 字典與列表、JavaScript 物件與陣列
 // 完成以下函式，正確計算出員工的平均薪資，請考慮員工數量會變動的情況。
-/*
 function avg(data) {
-
+    let salarySum = 0;
+    for (let i = 0; i < data.count; i++ ) {
+        let salary = data.employees[i].salary;
+        salarySum += salary;
+    }
+    let average = salarySum / data.count;
+    console.log(average);
 }
 avg({
     'count': 3,
@@ -40,7 +43,16 @@ avg({
 // 要求三：演算法
 // 找出至少包含兩筆整數的列表 (Python) 或陣列 (JavaScript) 中，兩兩數字相乘後的最大值。
 function maxProduct(nums) {
-    
+    let maxProduct = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let product = nums[i] * nums[j];
+            if (product > maxProduct) {
+                maxProduct = product;
+            }
+        }
+    }
+    console.log(maxProduct)  ;
 }
 
 maxProduct([5, 20, 2, 6]); // 得到 120 因為 20 和 6 相乘得到最大值
@@ -51,7 +63,12 @@ maxProduct([10, -20, 0, 3]); // 得到 30 因為 10 和 3 相乘得到最大值
 // specific target. You can assume that each input would have exactly one solution, and you
 // can not use the same element twice.
 function twoSum(nums, target) {
-
+    for (let i = 0 ; i < nums.length; i++ ) {
+        for (let j = i + 1; j < nums.length; j++ ) {
+            let twoSum = nums[i] + nums[j];
+            if (twoSum === target) return [i, j];
+        }
+    }
 }
 result = twoSum([2, 11, 7, 15], 9);
 console.log(result); // show [0, 2] because nums[0] + nums[2] is 9
@@ -59,6 +76,7 @@ console.log(result); // show [0, 2] because nums[0] + nums[2] is 9
 // 要求五 ( Optional )：演算法
 // 給定只會包含 0 或 1 兩種數字的列表 (Python) 或陣列 (JavaScript)，計算連續出現 0 的最大
 // 長度。
+/*
 function maxZeros(nums) {
 
 }
