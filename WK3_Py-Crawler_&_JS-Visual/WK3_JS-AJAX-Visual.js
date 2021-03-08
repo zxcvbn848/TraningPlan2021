d6be6024b3f701c408f6a609253ce44ad1110a92
zxcvbn848 <zxcvbn884488@@gmail.com>
2021-03-08 21:31:13 +0800

    All Push 2rd

--- WK3_Py-Crawler_&_JS-Visual/WK3_JS-AJAX-Visual.js
+++ WK3_Py-Crawler_&_JS-Visual/WK3_JS-AJAX-Visual.js
@@ -1,11 +1,9 @@
-// https://data.taipei/api/v1/dataset/36847f3f-deff-4183-a5bb-800737591de5?scope=resourceAquire
-// https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json
 
 let src = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
 
 fetch(src)
     .then(function(response) {
-        return response.json(); // 將資料用 JSON 的格式詮釋成：物件和陣列的組合
+        return response.json(); 
     })
     .then(function(result) {
         let scenicArea = result.result.results;
@@ -14,64 +12,34 @@ fetch(src)
         let titleArray = [];
         for (let i = 0; i < scenicArea.length; i++) {
             title = scenicArea[i].stitle;
-            // console.log(title);
             titleArray.push(title);
         }
-        // console.log(titleArray);
 
         let imageUrl;
         let imageUrlArray = [];
         for (let i = 0; i < scenicArea.length; i++) {
             imageUrl = scenicArea[i].file.split('.jpg', 2)[0] + '.jpg';
-            // console.log(title);
             imageUrlArray.push(imageUrl);
         }
-        // console.log(imageUrlArray);
 
         let boxes = document.getElementsByClassName('box');
         for (i = 0; i < scenicArea.length; i++) {
-            // createElement('div', 'box-image', boxImage, box);
-            // createElement('div', 'box-text', boxText, box);
             let box = boxes[i];
 
             let boxImage = document.createElement('div');
             boxImage.className = 'box-image';
-            imgTag = document.createElement('img');
+            let imgTag = document.createElement('img');
             imgTag.src = imageUrlArray[i];
+            
             boxImage.appendChild(imgTag);
             box.appendChild(boxImage);
     
             let boxText = document.createElement('div');
             boxText.className = 'box-text';
-            textContent = document.createTextNode(titleArray[i]);
-            boxText.appendChild(textContent);
-            box.appendChild(boxText);
-    
+            let textContent = document.createTextNode(titleArray[i]);
             
-            // createText('div', 'box-text', titleArray[0], textContent, boxText);    
-            // createImg('img', 'https://zxcvbn848.github.io/Picture/1.jpg?raw=true', img, boxImage);
-    
+            boxText.appendChild(textContent);
+            box.appendChild(boxText);                
         }
         document.body.appendChild(boxes);
     });
-
-
-// function createElement(tag, className, childTag, parentTag) {
-//     let childTag = document.createElement(tag);
-//     childTag.className = className;
-//     parentTag.appendChild(childTag);
-// }
-
-// function createImg(tag, src, imgTag, ChildTag) {
-//     imgTag = document.createElement(tag);
-
-//     imgTag.src = src;
-//     ChildTag.appendChild(imgTag);
-//     parentTag.appendChild(childTag);
-// }
-
-// function createText(tag, className, text, childTag, parentTag) {
-//     textNode = document.createTextNode(text);
-//     childTag.appendChild(textNode);
-//     parentTag.appendChild(childTag);
-// }
--- WK3_Py-Crawler_&_JS-Visual/WK3_Py-Crawler.py
+++ WK3_Py-Crawler_&_JS-Visual/WK3_Py-Crawler.py
@@ -1,5 +1,3 @@
-# https://data.taipei/api/v1/dataset/36847f3f-deff-4183-a5bb-800737591de5?scope=resourceAquire
-# https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json
 
 import json
 import urllib.request as req
@@ -7,16 +5,13 @@ url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json
 with req.urlopen(url) as response:
     data = json.load(response)
 scenicAreaList = data["result"]["results"]
-# title = "景點名稱, 經度, 緯度, 第一張圖檔網址"
 with open("data.txt", "w", encoding="utf-8") as file:
-    # file.write(title + "\n")
     for sa in scenicAreaList:
         stitle = sa["stitle"]
         longitude = sa["longitude"]
         latitude = sa["latitude"]
         imgUrl = sa["file"].split(".jpg", 1)[0]
         
-        info = f'{stitle}, {longitude}, {latitude}, {imgUrl}.jpg+'
-        print(info)
+        info = f'{stitle}, {longitude}, {latitude}, {imgUrl}.jpg'
         file.write(info + "\n")
 
