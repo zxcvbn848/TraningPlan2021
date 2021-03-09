620a0a0a78cc44e8eb8ab0b6a7c1bfc641d9120c
zxcvbn848 <zxcvbn884488@@gmail.com>
2021-03-09 20:48:27 +0800

    HTML JS optimized

--- WK3_Py-Crawler_&_JS-Visual/WK3_JS-AJAX-Visual.js
+++ WK3_Py-Crawler_&_JS-Visual/WK3_JS-AJAX-Visual.js
@@ -1,57 +1,18 @@
 
-var src = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
+let src = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
 
-var count = 0;
+let titleArray = [];
+let imageUrlArray = [];
+let count = 0;
 
-var titleArray = [];
-var imageUrlArray = [];
+load();
 
-fetch(src)
-    .then(function(response) {
-        return response.json(); 
-    })
-    .then(function(result) {       
-        let content = document.getElementsByClassName('content')[0];
-
-        let scenicArea = result.result.results;                  
-        
-        for (let j = count; j < count + 8; j++) {
-            let box = document.createElement('div');
-            box.className = 'box';
-            content.appendChild(box);
-
-            let title = scenicArea[j].stitle;
-            titleArray.push(title);
-
-            let imageUrl = 'http://' + scenicArea[j].file.split('http://')[1];
-            imageUrlArray.push(imageUrl);
-
-            let boxImage = document.createElement('div');
-            boxImage.className = 'box-image';
-            let imgTag = document.createElement('img');
-            imgTag.src = imageUrlArray[j];
-            
-            boxImage.appendChild(imgTag);
-            box.appendChild(boxImage);
-    
-            let boxText = document.createElement('div');
-            boxText.className = 'box-text';
-            let textContent = document.createTextNode(titleArray[j]);
-            
-            boxText.appendChild(textContent);
-            box.appendChild(boxText);
-        }
-        let boxes = document.getElementsByClassName('box');
-        content.appendChild(boxes);
-    });
-
-function loadMore() {
+function load() {
     fetch(src)
         .then(function(response) {
             return response.json(); 
         })
         .then(function(result) {
-            count += 8;
 
             let content = document.getElementsByClassName('content')[0];
             
@@ -83,7 +44,8 @@ function loadMore() {
                 boxText.appendChild(textContent);
                 box.appendChild(boxText);
             }
-            
+            count += 8;
+
             let boxes = document.getElementsByClassName('box');
             content.appendChild(boxes);
         });
