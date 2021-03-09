67bf95e5c2cc06a3ca9c843a72cf583fafb19f39
zxcvbn848 <zxcvbn884488@@gmail.com>
2021-03-09 14:56:05 +0800

    Load More function

--- WK3_Py-Crawler_&_JS-Visual/WK3_JS-AJAX-Visual.js
+++ WK3_Py-Crawler_&_JS-Visual/WK3_JS-AJAX-Visual.js
@@ -1,104 +1,91 @@
 
-let src = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
+var src = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
+
+var count = 0;
+
+var titleArray = [];
+var imageUrlArray = [];
 
 fetch(src)
     .then(function(response) {
         return response.json(); 
     })
     .then(function(result) {       
-        let scenicArea = result.result.results;
+        let content = document.getElementsByClassName('content')[0];
 
-        let title;
-        let titleArray = [];
-        for (let i = 0; i < scenicArea.length; i++) {
-            title = scenicArea[i].stitle;
+        let scenicArea = result.result.results;                  
+        
+        for (let j = count; j < count + 8; j++) {
+            let box = document.createElement('div');
+            box.className = 'box';
+            content.appendChild(box);
+
+            let title = scenicArea[j].stitle;
             titleArray.push(title);
-        }
 
-        let imageUrl;
-        let imageUrlArray = [];
-        for (let i = 0; i < scenicArea.length; i++) {
-            imageUrl = 'http://' + scenicArea[i].file.split('http://')[1];
+            let imageUrl = 'http://' + scenicArea[j].file.split('http://')[1];
             imageUrlArray.push(imageUrl);
-        }
-
-        let boxes = document.getElementsByClassName('box');
-        for (i = 0; i < scenicArea.length; i++) {
-            let box = boxes[i];
 
             let boxImage = document.createElement('div');
             boxImage.className = 'box-image';
             let imgTag = document.createElement('img');
-            imgTag.src = imageUrlArray[i];
+            imgTag.src = imageUrlArray[j];
             
             boxImage.appendChild(imgTag);
             box.appendChild(boxImage);
     
             let boxText = document.createElement('div');
             boxText.className = 'box-text';
-            let textContent = document.createTextNode(titleArray[i]);
+            let textContent = document.createTextNode(titleArray[j]);
             
             boxText.appendChild(textContent);
-            box.appendChild(boxText);                
+            box.appendChild(boxText);
         }
-        let content = document.getElementsByClassName('content')[0];
+        let boxes = document.getElementsByClassName('box');
         content.appendChild(boxes);
     });
 
 function loadMore() {
-    let content = document.getElementsByClassName('content')[0];
+    fetch(src)
+        .then(function(response) {
+            return response.json(); 
+        })
+        .then(function(result) {
+            count = count + 8;
+
+            let content = document.getElementsByClassName('content')[0];
+            
+            let scenicArea = result.result.results;            
+            
+            for (let j = count; j < count + 8; j++) {
+                let box = document.createElement('div');
+                box.className = 'box';
+                content.appendChild(box);
+
+                let title = scenicArea[j].stitle;
+                titleArray.push(title);
+
+                let imageUrl = 'http://' + scenicArea[j].file.split('http://')[1];
+                imageUrlArray.push(imageUrl);
     
-    for (let j = 0; j < 8; j++) {
-        let box = document.createElement('div');
-        box.className = 'box';
-        content.appendChild(box);
+                let boxImage = document.createElement('div');
+                boxImage.className = 'box-image';
+                let imgTag = document.createElement('img');
+                imgTag.src = imageUrlArray[j];
+                
+                boxImage.appendChild(imgTag);
+                box.appendChild(boxImage);
+        
+                let boxText = document.createElement('div');
+                boxText.className = 'box-text';
+                let textContent = document.createTextNode(titleArray[j]);
+                
+                boxText.appendChild(textContent);
+                box.appendChild(boxText);
+                
+            }
+            
+            let boxes = document.getElementsByClassName('box');
+            content.appendChild(boxes);
+        });
     }
-
-let src = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
-
-fetch(src)
-    .then(function(response) {
-        return response.json(); 
-    })
-    .then(function(result) {       
-        let scenicArea = result.result.results;
-        var scenicNumber = 8;
-
-        let title;
-        let titleArray = [];
-        for (let i = scenicNumber; i < scenicArea.length; i++) {
-            title = scenicArea[i].stitle;
-            titleArray.push(title);
-        }
-
-        let imageUrl;
-        let imageUrlArray = [];
-        for (let i = scenicNumber; i < scenicArea.length; i++) {
-            imageUrl = 'http://' + scenicArea[i].file.split('http://')[1];
-            imageUrlArray.push(imageUrl);
-        }
-
-        let boxes = document.getElementsByClassName('box');
-        for (let i = scenicNumber; i < scenicArea.length; i++) {
-            let box = boxes[i];
-
-            let boxImage = document.createElement('div');
-            boxImage.className = 'box-image';
-            let imgTag = document.createElement('img');
-            imgTag.src = imageUrlArray[i];
-            
-            boxImage.appendChild(imgTag);
-            box.appendChild(boxImage);
-    
-            let boxText = document.createElement('div');
-            boxText.className = 'box-text';
-            let textContent = document.createTextNode(titleArray[i]);
-            
-            boxText.appendChild(textContent);
-            box.appendChild(boxText);                
-        }
-
-        let content = document.getElementsByClassName('content')[0];
-        content.appendChild(boxes);
-    });
-}
